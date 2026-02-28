4ba751998315e2fb74ee462ae4a2b36189ee2f73
ibuki-kamiza <shangjingyichui@gmail.com>
2026-02-28 13:59:18 +0900

    first commit

---
+++ src/main/java/com/example/demo/CytechECsiteTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CytechECsiteTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CytechECsiteTestApplication.class, args);
+	}
+
+}
